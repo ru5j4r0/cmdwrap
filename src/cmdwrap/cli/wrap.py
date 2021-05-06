dc41a9c507ae1dd2ec85520313e1a1afef4b210f
ru5j4r0 <59990307+ru5j4r0@users.noreply.github.com>
2021-05-06 16:06:52 +0900

    refactoring

--- src/cmdwrap/cli/wrap.py
+++ src/cmdwrap/cli/wrap.py
@@ -3,37 +3,40 @@ import os
 import stat
 
 def add_ux(file_name):
-    s = os.stat(file_name).st_mode
-    os.chmod(file_name, s | stat.S_IXUSR)
+    mode_old = os.stat(file_name).st_mode
+    mode_new = mode_old | stat.S_IXUSR
+    os.chmod(file_name, mode_new)
 
-def init(fd, cmd):
-    fd.write(
-f"""#!/usr/bin/env python3
+def template(cmd):
+    return f"""#!/usr/bin/env python3
 
 import cmdwrap
+
+BASE_CMD = {cmd}
 
 ROUTING = {{
 }}
 
 if __name__ == '__main__':
-    cmdwrap.wrap(ROUTING{cmd})
+    cmdwrap.main(ROUTING, BASE_CMD)
 
 """
-    )
+
+def create_file(file_name, cmd):
+    with open(file_name, 'w') as fd:
+        add_ux(file_name)
+        src = template(cmd)
+        fd.write(src)
 
 def main():
-    argc = len(sys.argv)
-
-    if argc == 2 or argc == 3:
+    if (argc := len(sys.argv)) > 1:
         file_name = sys.argv[1]
 
-        if argc == 3:
-            cmd = sys.argv[2]
-            cmd = f", '{cmd}'"
+        if argc > 2:
+            cmd = ' '.join(sys.argv[2:])
+            cmd = f"'{cmd}'"
         else:
-            cmd = ''
+            cmd = None
 
-        with open(file_name, 'w') as fd:
-            add_ux(file_name)
-            init(fd, cmd)
+        create_file(file_name, cmd)
 
--- src/cmdwrap/main.py
+++ src/cmdwrap/main.py
@@ -5,12 +5,14 @@ from itertools import chain
 def _head_rest(head = None, *rest):
     return head, list(rest)
 
+def _value_type(d, key):
+    return (v := d[key]), type(v)
+
 def _route(d, args):
     head, rest = _head_rest(*args)
 
     if head in d.keys():
-        v = d[head]
-        vt = type(v)
+        v, vt = _value_type(d, head)
 
         if vt == str:
             args[0] = v
@@ -25,18 +27,20 @@ def _route(d, args):
 
     return args
 
-def _preproc_args(args):
-    return list(chain.from_iterable(map(str.split, args)))
+def _flatten(l):
+    c = chain.from_iterable(l)
+    return list(c)
 
-def wrap(routing, cmd = None):
+def _preproc_args(args):
+    splitted_args = map(str.split, args)
+    return _flatten(splitted_args)
+
+def main(routing, cmd = None):
     args = _route(routing, sys.argv[1:])
     args = _preproc_args(args)
 
     if cmd:
-        run([cmd, *args])
-    else:
-        if len(args) == 0:
-            run('true')
-        else:
-            run([*args])
+        run([*cmd.split(), *args])
+    elif len(args) > 0:
+        run([*args])
 
